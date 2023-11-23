28016f24f3009c96e4e0b38ee848586c3ed780e5
dp <dp.dev.im@gmail.com>
2023-11-24 07:39:24 +0900

    :sparkles: mall_sample: payment_request, payment_pay

--- mall_sample/urls.py
+++ mall_sample/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("payment/request/", views.payment_request, name="payment_request"),
+    path("payment/<int:pk>/pay/", views.payment_pay, name="payment_pay"),
+    path("payment/<int:pk>/check/", views.payment_check, name="payment_check"),
 ]
