9b8a840a5733f56aec2e233e0e34e1f42a6c3846
dp <dp.dev.im@gmail.com>
2023-11-27 02:56:05 +0900

    :sparkles: [mall-app] admin.py

--- mall/admin.py
+++ mall/admin.py
@@ -4,9 +4,22 @@ from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["pk", "name"]
+    list_display_links = ["name"]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["pk", "category", "name", "price", "status"]
+    list_display_links = ["name"]
+    search_fields = ["name"]
+    list_filter = ["category", "status", "created_at", "updated_at"]
+    date_hierarchy = "updated_at"
+    actions = ["make_active"]
+
+    @admin.display(description=f"지정된 상품을 {Product.Status.ACTIVE.label} 상태로 변경")
+    def make_active(self, request, queryset):
+        count = queryset.update(status=Product.Status.ACTIVE)
+        self.message_user(
+            request, f"{count}개의 상품을 {Product.Status.ACTIVE.label} 상태로 변경 완료"
+        )
