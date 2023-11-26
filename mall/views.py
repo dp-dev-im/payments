d77039e6ffbb6a7eb789cc5088e403ddf75a1603
dp <dp.dev.im@gmail.com>
2023-11-27 05:27:06 +0900

    :sparkles: add: [mall] - product_list

--- mall/views.py
+++ mall/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from mall.models import Product
+
+
 # Create your views here.
+def product_list(request):
+    product_qs = Product.objects.all().select_related("category")
+    return render(
+        request,
+        "mall/product_list.html",
+        {
+            "product_list": product_qs,
+        },
+    )
