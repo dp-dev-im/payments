29af25ccfb16933dbfdfe51718d2b1f02fe3a8fa
dp <dp.dev.im@gmail.com>
2023-11-26 19:53:25 +0900

    :sparkles: add: check logic

--- mall_sample/urls.py
+++ mall_sample/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("payment/request/", views.payment_request, name="payment_request"),
     path("payment/<int:pk>/pay/", views.payment_pay, name="payment_pay"),
     path("payment/<int:pk>/check/", views.payment_check, name="payment_check"),
+    path("payment/<int:pk>/", views.payment_detail, name="payment_detail"),
 ]
