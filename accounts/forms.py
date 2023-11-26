4171031a10fc9268ed77de66f8390d6fece672c8
dp <dp.dev.im@gmail.com>
2023-11-26 21:29:14 +0900

    :sparkles: add: login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
 
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
+
+
+class LoginForm(AuthenticationForm):
+    pass
