70b2374781f6b1c11c8f90aa07859276c88bc29b
dp <dp.dev.im@gmail.com>
2023-11-27 05:16:01 +0900

    :bug: fix: config/settings.py document_root

--- config/urls.py
+++ config/urls.py
@@ -16,4 +16,4 @@ if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
-    static(settings.MEDIA_URL, document_url=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
