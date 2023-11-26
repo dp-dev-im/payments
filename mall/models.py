e01f10549e6cbe24b5ddad7fc2a870326f9e705a
dp <dp.dev.im@gmail.com>
2023-11-26 23:32:03 +0900

    :sparkles: mall- models: Category, Product

--- mall/models.py
+++ mall/models.py
@@ -1,3 +1,44 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = "product categorization"
+
+
+class Product(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = "a", "정상"
+        SOLD_OUT = "s", "품절"
+        OBSOLETE = "o", "단종"
+        INACTIVE = "i", "비활성화"
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+
+    name = models.CharField(max_length=80, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(
+        validators=[MinValueValidator(100, message="minimum: 100")]
+    )
+    status = models.CharField(
+        max_length=1,
+        choices=Status.choices,
+        default=Status.INACTIVE,
+    )
+    photo = models.ImageField(upload_to="mall/product/photo/%Y/%m/%d/")
+
+    def __str__(self):
+        return f"<{self.pk}> {self.name}"
+
+    class Meta:
+        verbose_name = verbose_name_plural = "product"
