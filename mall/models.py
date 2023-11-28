3c97cab67cfe11c7b85e22a7fb65314da2fea15c
dp <dp.dev.im@gmail.com>
2023-11-28 23:15:03 +0900

    :sparkles: [mall] - models: Order, OrderedProduct

--- mall/models.py
+++ mall/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models import UniqueConstraint
@@ -75,3 +77,60 @@ class CartProduct(models.Model):
         constraints = [
             UniqueConstraint(fields=["user", "product"], name="unique_user_and_product")
         ]
+
+
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        REQUESTED = "requested", "주문요청"
+        FAILED_PAYMENT = "failed_payment", "결제실패"
+        PAID = "paid", "결제 완료"
+        PREPARED_PRODUCT = "prepared_product", "상품준비중"
+        SHIPPED = "shipped", "배송중"
+        DELIVERED = "delivered", "배송완료"
+        CANCELED = "canceled", "주문취소"
+
+    uid = models.UUIDField(default=uuid4, editable=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    total_amount = models.PositiveIntegerField("결제금액")
+    status = models.CharField(
+        "진행상태",
+        max_length=18,
+        choices=StatusChoices.choices,
+        default=StatusChoices.REQUESTED,
+        db_index=True,
+    )
+    product_set = models.ManyToManyField(
+        Product,
+        through="OrderedProduct",
+        blank=False,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderedProduct(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    name = models.CharField("상품명", max_length=90, help_text="주문 시점 상품명 저장")
+    price = models.PositiveIntegerField(
+        "상품가격",
+        help_text="주문 시점 상품 가격",
+        validators=[MinValueValidator(100, message="minimum: 100")],
+    )
+    quantity = models.PositiveIntegerField(
+        validators=[MinValueValidator(1, message="1")]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
